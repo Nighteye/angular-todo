01147e90c9f54293e8a9631ed84bee0e3f9e1fd8
Grégoire <gregoire.nieto@zaclys.net>
2025-11-20 16:54:20 +0100

    complete tuto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,39 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Item } from "./item";
+import { ItemComponent } from "./item/item"
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [CommonModule]
+  imports: [CommonModule, ItemComponent]
 })
 export class App {
-  componentTitle = "My To Do List";
+  componentTitle = "Ma liste de tâches à accomplir";
 
   filter: "all" | "active" | "done" = "all";
 
   allItems = [
-    { description: "eat", done: true },
-    { description: "sleep", done: false },
-    { description: "play", done: false },
-    { description: "laugh", done: false },
+    { description: "manger", done: true },
+    { description: "dormir", done: false },
+    { description: "jouer", done: false },
+    { description: "rire", done: false },
   ];
 
   get items() {
-    if(this.filter === "all") {
+    if (this.filter === "all") {
       return this.allItems;
     }
-    return this.allItems.filter((item) => {
-      this.filter === "done" ? item.done : !item.done;
-    });
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
   }
-
   addItem(description: string) {
     if(!description) return;
     this.allItems.unshift({
       description,
       done: false
     });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
   }
 }
